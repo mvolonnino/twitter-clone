c3e26f076fa3d730f2f6ce21a706ade0f981b053
Matt Volonnino <mvolonnino12@gmail.com>
2020-10-14 15:13:08 -0400

    init repo with starting of sidebar, sidebarOption components.

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./Sidebar.css";
+import SidebarOption from "../SidebarOption/SidebarOption";
+
+// icons
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter Icon */}
+      <TwitterIcon />
+      {/* SidbarOption */}
+      <SidebarOption active Icon={HomeIcon} text="Home" />
+      {/* SidbarOption */}
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      {/* SidbarOption */}
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      {/* SidbarOption */}
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      {/* SidbarOption */}
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      {/* SidbarOption */}
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      {/* SidbarOption */}
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      {/* SidbarOption */}
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+      {/* BUtton -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
