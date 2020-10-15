66270853261c96393be5b620e60bb2a79cf069ff
Matt Volonnino <mvolonnino12@gmail.com>
2020-10-15 10:35:10 -0400

    added widgets panel that utilizes twitter react embed

--- src/components/Widgets/Widgets.jsx
+++ src/components/Widgets/Widgets.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 import "./Widgets.css";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets_input">
+        <SearchIcon className="widgets_searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets_widgetContainer">
+        <h2>What's Happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1316529163671678977"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="Marvel"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://www.facebook.com/matt.volonnino/"}
+          options={{ text: "#reactjs is awesome", via: "MattVolonni1" }}
+        />
+      </div>
     </div>
   );
 }
